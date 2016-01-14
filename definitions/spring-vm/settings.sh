c9330a83226f15e1ecd01ca46a87320fbe8a5711
abma <spring@abma.de>
2016-01-14 07:56:30 +0100

    fix retrieving url of current release / retrieve only once at build time

--- definitions/spring-vm/settings.sh
+++ definitions/spring-vm/settings.sh
@@ -1,18 +1,20 @@
 # settings that will be shared between all scripts
 
-cat <<DATAEOF > "/etc/profile.d/veewee.sh"
-# stage 3 filename and full url
-# retrieve from Gentoo current autobuild txt - these change regularly
-
 # distfiles source
 distfiles_url=http://distfiles.gentoo.org
 
 build_arch="amd64"
 build_proc="amd64"
-stage3current=\`curl -s \${distfiles_url}/releases/\${build_arch}/autobuilds/latest-stage3-\${build_proc}.txt|grep -v "^#"\`
-export stage3url="\${distfiles_url}/releases/\${build_arch}/autobuilds/\${stage3current}"
-export stage3file=\${stage3current##*/}
-export portageurl="\${distfiles_url}/snapshots/portage-latest.tar.bz2"
+
+# stage 3 filename and full url
+# retrieve from Gentoo current autobuild txt - these change regularly
+stage3current=$(curl -s ${distfiles_url}/releases/${build_arch}/autobuilds/latest-stage3-${build_proc}.txt |egrep -o '^[^  ^#]+')
+
+cat <<DATAEOF > "/etc/profile.d/veewee.sh"
+
+export stage3url="${distfiles_url}/releases/${build_arch}/autobuilds/${stage3current}"
+export stage3file=${stage3current##*/}
+export portageurl="${distfiles_url}/snapshots/portage-latest.tar.bz2"
 
 # these two (configuring the compiler) and the stage3 url can be changed to build a 32 bit system
 export accept_keywords="amd64"
