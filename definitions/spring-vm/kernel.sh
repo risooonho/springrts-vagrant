1f37dd3de4154ad14c894482cc87950fb16c2036
abma <spring@abma.de>
2016-01-15 22:36:39 +0100

    typos

--- definitions/spring-vm/kernel.sh
+++ definitions/spring-vm/kernel.sh
@@ -12,7 +12,6 @@ dev-util/kbuild ~x86 ~amd64
 DATAEOF
 
 # get, configure, compile and install the kernel and modules
-chroot "$chroot" /bin/bash <<DATAEOF
-emerge =sys-kernel/gentoo-sources sys-kernel/genkernel app-portage/gentoolkit
+emerge sys-kernel/gentoo-sources sys-kernel/genkernel app-portage/gentoolkit
 
 genkernel --install --symlink
