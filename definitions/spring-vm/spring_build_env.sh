a4116fcb8ac73205b2019dd47932511b6da2ff0b
Eshed <ashdnazg@gmail.com>
2016-10-18 02:19:28 +0300

    more updates

--- definitions/spring-vm/spring_build_env.sh
+++ definitions/spring-vm/spring_build_env.sh
@@ -15,6 +15,7 @@ media-libs/libogg static-libs
 media-libs/freetype static-libs autohinter
 media-libs/glew static-libs
 media-libs/libpng static-libs
+media-libs/openal static-libs
 x11-libs/libXcursor static-libs
 app-arch/bzip2 static-libs
 x11-libs/libXdmcp static-libs
@@ -49,8 +50,9 @@ media-libs/libsdl2 spring-static-buildslave
 media-libs/libvorbis spring-static-buildslave
 media-libs/freetype spring-static-buildslave
 media-libs/glew spring-static-buildslave
-sys-devel/gcc spring-static-buildslave
+media-libs/openal spring-static-buildslave
 media-libs/libpng spring-static-buildslave
+sys-devel/gcc spring-static-buildslave
 x11-libs/libXcursor spring-static-buildslave
 DATAEOF
 
@@ -64,7 +66,7 @@ chroot "$chroot" /bin/bash <<DATAEOF
 emerge sys-libs/zlib media-libs/devil dev-libs/boost media-libs/jpeg media-libs/libsdl2 \
 media-libs/libvorbis media-libs/freetype media-libs/glew media-libs/libpng x11-libs/libXcursor \
 app-arch/bzip2 x11-libs/libXdmcp x11-libs/libXrender x11-libs/libX11 x11-libs/libXfixes \
-x11-libs/libxcb x11-libs/libXau net-misc/curl sys-libs/libunwind virtual/jdk
+x11-libs/libxcb x11-libs/libXau net-misc/curl sys-libs/libunwind virtual/jdk app-arch/p7zip media-libs/openal
 DATAEOF
 
 
