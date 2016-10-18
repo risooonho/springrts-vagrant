eb7e8abae764ef3f46ea89d415ff1edf5f281042
Eshed <ashdnazg@gmail.com>
2016-10-18 18:08:30 +0300

    add libunwind fix and expat/fontconfig settings

--- definitions/spring-vm/spring_build_env.sh
+++ definitions/spring-vm/spring_build_env.sh
@@ -9,6 +9,7 @@ media-libs/devil static-libs opengl png jpeg tiff gif X
 media-libs/giflib static-libs
 media-libs/tiff static-libs
 dev-libs/boost static-libs python
+dev-libs/expat static-libs
 media-libs/libjpeg-turbo  static-libs
 media-libs/libsdl2 -sound opengl static-libs X
 media-libs/libvorbis static-libs
@@ -16,6 +17,7 @@ media-libs/libogg static-libs
 media-libs/freetype static-libs autohinter
 media-libs/glew static-libs
 media-libs/libpng static-libs
+media-libs/fontconfig static-libs
 x11-libs/libXcursor static-libs
 app-arch/bzip2 static-libs
 x11-libs/libXdmcp static-libs
@@ -53,6 +55,7 @@ media-libs/libvorbis spring-static-buildslave
 media-libs/freetype spring-static-buildslave
 media-libs/glew spring-static-buildslave
 media-libs/libpng spring-static-buildslave
+media-libs/fontconfig spring-static-buildslave
 sys-devel/gcc spring-static-buildslave
 x11-libs/libXcursor spring-static-buildslave
 DATAEOF
@@ -68,6 +71,13 @@ chroot "$chroot" /bin/bash <<DATAEOF
 emerge sys-libs/zlib media-libs/devil dev-libs/boost media-libs/libjpeg-turbo media-libs/libsdl2 \
 media-libs/libvorbis media-libs/freetype media-libs/glew media-libs/libpng x11-libs/libXcursor \
 app-arch/bzip2 x11-libs/libXdmcp x11-libs/libXrender x11-libs/libX11 x11-libs/libXfixes \
-x11-libs/libxcb x11-libs/libXau net-misc/curl sys-libs/libunwind dev-java/icedtea-bin app-arch/p7zip media-libs/openal
+x11-libs/libxcb x11-libs/libXau net-misc/curl sys-libs/libunwind dev-java/icedtea-bin app-arch/p7zip media-libs/openal \
+dev-libs/expat media-libs/fontconfig
 DATAEOF
 
+# hack libunwind to prevent multiple definition of '_Unwind_Resume'
+chroot "$chroot" /bin/bash <<DATAEOF
+sed -i '/--enable-cxx-exceptions/d' /usr/portage/sys-libs/libunwind/libunwind-1.1-r1.ebuild
+ebuild /usr/portage/sys-libs/libunwind/libunwind-1.1-r1.ebuild digest
+ebuild /usr/portage/sys-libs/libunwind/libunwind-1.1-r1.ebuild qmerge
+DATAEOF
