32a3cf5d53aeff17e0d2068e3a7e96a43722832a
abma <spring@abma.de>
2016-11-02 22:00:12 +0100

    add ccache/ninje required for buildslave

--- definitions/spring-vm/spring_buildbot.sh
+++ definitions/spring-vm/spring_buildbot.sh
@@ -3,7 +3,7 @@ source /etc/profile
 
 
 chroot "$chroot" /bin/bash <<DATAEOF
-emerge dev-util/buildbot-slave
+emerge dev-util/buildbot-slave dev-util/ninja dev-util/ccache
 useradd -s /bin/false -d /home/buildbot -m buildbot
 eselect python set python2.7
 DATAEOF
