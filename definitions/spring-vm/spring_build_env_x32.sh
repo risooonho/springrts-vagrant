e8b9472630e21b8f021d31aa5e0ea6e7a68944eb
abma <spring@abma.de>
2016-11-03 02:11:54 +0100

    "revert" to wiki :-|

--- definitions/spring-vm/spring_build_env_x32.sh
+++ definitions/spring-vm/spring_build_env_x32.sh
@@ -9,18 +9,36 @@ DATAEOF
 
 mkdir -p "$chroot/usr/i686-pc-linux-gnu/etc/portage/package.use"
 cat <<DATAEOF >  "$chroot/usr/i686-pc-linux-gnu/etc/portage/package.use/spring-static-buildslave"
-sys-libs/zlib minizip
-media-libs/devil opengl png jpeg tiff gif X
-media-libs/tiff -cxx
-dev-libs/boost -python
-media-libs/libsdl2 -audio opengl X
-media-libs/freetype autohinter
-dev-java/oracle-jdk-bin -X -fontconfig
+sys-libs/zlib static-libs minizip
+media-libs/devil static-libs opengl png jpeg tiff gif X
+media-libs/giflib static-libs
+media-libs/tiff static-libs
+dev-libs/boost static-libs python
+dev-libs/expat static-libs
+media-libs/libjpeg-turbo  static-libs
+media-libs/libsdl2 -sound opengl static-libs X
+media-libs/libvorbis static-libs
+media-libs/libogg static-libs
+media-libs/freetype static-libs autohinter
+media-libs/glew static-libs
+media-libs/libpng static-libs
+media-libs/fontconfig static-libs
+x11-libs/libXcursor static-libs
+app-arch/bzip2 static-libs
+x11-libs/libXdmcp static-libs
+x11-libs/libXrender static-libs
+x11-libs/libX11 static-libs
+x11-libs/libXfixes static-libs
+x11-libs/libxcb static-libs
+x11-libs/libXau static-libs
+net-misc/curl static-libs
+dev-libs/openssl static-libs
+sys-libs/libunwind static-libs
 DATAEOF
 
 cat <<DATAEOF >> "$chroot/usr/i686-pc-linux-gnu/etc/portage/make.conf"
 PYTHON_TARGETS="python2_7"
-USE="${USE} -pam -llvm -tools -uuid -kmod -classic -dri3 -egl -gallium -gbm -nptl -udev -fortran static-libs"
+USE="${USE} -pam -llvm -tools -uuid -kmod -classic -dri3 -egl -gallium -gbm -nptl -udev -fortran"
 DATAEOF
 
 mkdir -p "$chroot/usr/i686-pc-linux-gnu/etc/portage/package.mask"
@@ -30,44 +48,13 @@ cat <<DATAEOF > "$chroot/usr/i686-pc-linux-gnu/etc/portage/package.mask/spring-s
 >=sys-libs/libunwind-1.0
 DATAEOF
 
-# because of --nodeps order of packages matter!
-
 chroot "$chroot" /bin/bash <<DATAEOF
-ARCH=x86 emerge-i686-pc-linux-gnu --nodeps \
-dev-libs/libffi \
-x11-misc/util-macros \
-dev-libs/glib \
-dev-util/pkgconfig \
-sys-libs/zlib \
-app-arch/bzip2 \
-dev-libs/boost \
-media-libs/tiff \
-media-libs/jpeg \
-media-libs/libvorbis \
-media-libs/freetype \
-media-libs/libpng \
-media-libs/openal \
-dev-libs/openssl \
-dev-lang/python \
-net-misc/curl \
-sys-libs/libunwind \
-x11-libs/libXdmcp \
-x11-libs/libXau \
-=x11-libs/libxcb-1.10 \
-x11-libs/libX11 \
-x11-libs/libXrender \
-x11-proto/fixesproto \
-x11-libs/libXfixes \
-x11-libs/libXcursor \
-x11-libs/libdrm \
-x11-proto/glproto \
-x11-libs/libXext  \
-x11-libs/libXdamage \
-x11-proto/dri2proto \
-media-libs/mesa \
-media-libs/glu \
-media-libs/glew \
-media-libs/devil \
-media-libs/libsdl2
+ARCH=x86 emerge dev-util/pkgconfig x11-misc/util-macros
+ARCH=x86 emerge-i686-pc-linux-gnu \
+sys-libs/zlib media-libs/devil dev-libs/boost media-libs/libjpeg-turbo media-libs/libsdl2 \
+media-libs/libvorbis media-libs/freetype media-libs/glew media-libs/libpng x11-libs/libXcursor \
+app-arch/bzip2 x11-libs/libXdmcp x11-libs/libXrender x11-libs/libX11 x11-libs/libXfixes \
+x11-libs/libxcb x11-libs/libXau net-misc/curl dev-java/icedtea-bin app-arch/p7zip media-libs/openal \
+dev-libs/expat media-libs/fontconfig sys-libs/libunwind
 DATAEOF
 
